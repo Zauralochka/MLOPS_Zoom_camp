ee550b56c7dc3d3321a3e46839ae489453de9f39
zauralochka <olgakurgan@gmail.com>
2023-06-04 23:43:40 +1000

    Updated Week 2 and added some files

--- Week_2/hpo.py
+++ Week_2/hpo.py
@@ -50,8 +50,8 @@ def run_optimization(data_path: str, num_trials: int):
             rf.fit(X_train, y_train)
             y_pred = rf.predict(X_val)
             rmse = mean_squared_error(y_val, y_pred, squared=False)
-            mlflow.log_metrics("rmse", rmse)
-            mlflow.log_artifact(local_path='models/',artifact_path='artifacts')
+            mlflow.log_metric("rmse", rmse)
+            
 
         return rmse
 
